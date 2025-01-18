1269a8475e6b1258f9b38e2a625ac989d5bfd217
minkyong00 <minkyong131@gmail.com>
2025-01-18 18:32:00 +0900

    showu 팀 개설 페이지 create 기능 추가

--- src/pages/showu/team/teamCreate/TeamCreatedContainer.js
+++ src/pages/showu/team/teamCreate/TeamCreatedContainer.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import S from './style';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useForm } from 'react-hook-form';
 
 const TeamCreatedContainer = () => {
   const navigate = useNavigate();
+  const [filesPath, setFilesPath] = useState(null);
+  const [fileName, setFileName] = useState(''); // 선택한 파일 이름
+  const { currentUser } = useSelector((state) => state.user);
+  const jwtToken = localStorage.getItem("jwtToken");
+
+  console.log("filesPath", filesPath)
+  console.log("fileName", fileName)
+
+  const { 
+    register, 
+    handleSubmit, 
+    setValue,
+    formState: { isSubmitting }
+  } = useForm({ mode: "onSubmit" });
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    console.log("selectedFile", selectedFile)
+    if (selectedFile) {
+      setFileName(selectedFile.name); // 선택된 파일 이름을 상태에 저장
+    } else {
+      setFileName(''); // 파일이 선택되지 않은 경우 상태 초기화
+    }
+  };
 
   return (
     <S.Wrapper>
@@ -16,7 +42,53 @@ const TeamCreatedContainer = () => {
 
       <S.FormContainer>
         <S.Fieldset>
-          <S.Form>
+          <S.Form onSubmit={handleSubmit(async (data) => {
+            console.log("form 제출")
+            console.log("data", data)
+            const formData = new FormData();
+
+
+            //파일 추가
+            const fileInput = document.getElementById('file');
+            const selectedFile = fileInput.files[0];
+            if(selectedFile){
+              formData.append("file", selectedFile)
+            }
+
+            formData.append("teamName", data.teamName);
+            formData.append("categoty", data.categoty);
+            formData.append("teamTitle", data.teamTitle);
+            formData.append("teamIntro", data.teamIntro);
+            formData.append("area", data.area);
+            formData.append("activityPeriodStart", data.activityPeriodStart);
+            formData.append("deadLine", data.deadLine);
+            formData.append("careerHistory", data.careerHistory);
+            formData.append("recruit", data.recruit);
+
+            await fetch(`http://localhost:8000/showu/team/create`, {
+              method: "POST",
+              headers: {
+                'Authorization': `Bearer ${jwtToken}`
+              },
+              body: formData  
+            })
+              .then((res) => res.json())
+              .then((res) => {
+                if(!res.teamCreateSuccess){
+                  console.log(res.message)
+                }
+                console.log("filepath", res.filePath)
+                const newFilePath = `http://localhost:8000${res.filePath}`
+                setFilesPath(newFilePath);
+                console.log("newFilePath", newFilePath)
+                alert(res.message);
+                console.log("팀 개설 완료");
+              })
+              .catch((error) => {
+                console.error("팀 개설 중 오류 발생", error)
+                alert("팀 개설 중 오류가 발생했습니다")
+              })
+          })} >
 
             <div>
               <S.Label htmlFor='teamName'>
@@ -26,51 +98,56 @@ const TeamCreatedContainer = () => {
                   name='teamName'
                   id='teamName'
                   placeholder='팀 이름을 입력하세요'
+                  {...register("teamName", {required : true})}
                 />
               </S.Label>
             </div>
 
             <div>
-              <S.Label htmlFor='startDate'>
+              <S.Label htmlFor='activityPeriodStart'>
                 <p>팀 활동 시작일</p>
                 <input 
                   type='date'
-                  name='startDate'
-                  id='startDate'
+                  name='activityPeriodStart'
+                  id='activityPeriodStart'
+                  {...register("activityPeriodStart", {required : true})}
                  />
               </S.Label>
             </div>
 
             <div>
-              <S.Label htmlFor='endDate'>
+              <S.Label htmlFor='deadLine'>
                 <p>팀 공고 마감일</p>
                 <input 
                   type='date'
-                  name='endDate'  
-                  id='endDate'  
+                  name='deadLine'  
+                  id='deadLine'
+                  {...register("deadLine", {required : true})}
                 />
               </S.Label>
             </div>
 
             <div>
-              <S.Label htmlFor='title'>
+              <S.Label htmlFor='teamTitle'>
                 <p>한줄 소개</p>
                 <input 
                   type='text'
-                  name='title'
-                  id='title'
+                  name='teamTitle'
+                  id='teamTitle'
                   placeholder='한줄 소개를 입력해주세요'
+                  {...register("teamTitle", {required : true})}
                 />
               </S.Label>
             </div>
 
             <div>
-              <S.LabelTextAarea htmlFor='intro'>
+              <S.LabelTextAarea htmlFor='teamIntro'>
                 <p>상세 소개</p>
                 <textarea  
-                  name='intro'
-                  id='intro'
+                  name='teamIntro'
+                  id='teamIntro'
                   placeholder='상세 소개를 입력해주세요'
+                  {...register("teamIntro", {required : true})}
                 />
               </S.LabelTextAarea>
             </div>
@@ -83,6 +160,7 @@ const TeamCreatedContainer = () => {
                   name='area'
                   id='area'
                   placeholder='ex)서울시, 중랑구'
+                  {...register("area", {required : true})}
                 />
               </S.Label>
             </div>
@@ -94,7 +172,8 @@ const TeamCreatedContainer = () => {
                   type='text'
                   name='category'  
                   id='category'
-                  placeholder='연기, 음악, 마술 중 선택하여 작성해주세요' 
+                  placeholder='연기, 음악, 마술 중 선택하여 작성해주세요'
+                  {...register("categoty", {required : true})}
                 />
               </S.Label>
             </div>
@@ -107,6 +186,7 @@ const TeamCreatedContainer = () => {
                   name='recruit'
                   id='recruit'
                   placeholder='모집 인원수를 입력해주세요 ex)1'
+                  {...register("recruit", {required : true})}
                 />
               </S.Label>
             </div>
@@ -119,23 +199,30 @@ const TeamCreatedContainer = () => {
                   name='careerHistory'  
                   id='careerHistory'
                   placeholder='경력무관, 신입, 경력 중 선택하여 작성해주세요'
+                  {...register("careerHistory", {required : true})}
                 />
               </S.Label>
             </div>
 
             <S.Portfolio>
-              <S.Label htmlFor='portfilo'>
+              <S.Label htmlFor='file'>
                 <p>포트폴리오</p>
                 <input 
                   type='file'
-                  name='portfilo'
-                  id='portfilo'
+                  name='file'
+                  id='file'
+                  {...register("file")}
+                  onChange={(e) => {
+                    handleFileChange(e);
+                  }}
                 />
+                <span>{fileName ? `${fileName}` : '+자료첨부'}</span>
               </S.Label>
             </S.Portfolio>
 
             <S.Button
               className='submit'
+              disabled={isSubmitting}
             >
               작성 완료
             </S.Button>
--- src/pages/showu/team/teamCreate/style.js
+++ src/pages/showu/team/teamCreate/style.js
@@ -61,7 +61,7 @@ const S = {};
       border: none;
     }
 
-    & input#startDate::-webkit-calendar-picker-indicator {
+    & input#activityPeriodStart::-webkit-calendar-picker-indicator {
       background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="%23FFFFFF"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M128 0c17.7 0 32 14.3 32 32l0 32 128 0 0-32c0-17.7 14.3-32 32-32s32 14.3 32 32l0 32 48 0c26.5 0 48 21.5 48 48l0 48L0 160l0-48C0 85.5 21.5 64 48 64l48 0 0-32c0-17.7 14.3-32 32-32zM0 192l448 0 0 272c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 192zm64 80l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm128 0l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0zM64 400l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0zm112 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16z"/></svg>');
       background-repeat: no-repeat;
       background-position: center;
@@ -77,7 +77,7 @@ const S = {};
       color: #fff;
     }
 
-    & input#endDate::-webkit-calendar-picker-indicator {
+    & input#deadLine::-webkit-calendar-picker-indicator {
       background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="%23FFFFFF"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M128 0c17.7 0 32 14.3 32 32l0 32 128 0 0-32c0-17.7 14.3-32 32-32s32 14.3 32 32l0 32 48 0c26.5 0 48 21.5 48 48l0 48L0 160l0-48C0 85.5 21.5 64 48 64l48 0 0-32c0-17.7 14.3-32 32-32zM0 192l448 0 0 272c0 26.5-21.5 48-48 48L48 512c-26.5 0-48-21.5-48-48L0 192zm64 80l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm128 0l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0zM64 400l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0zm112 16l0 32c0 8.8 7.2 16 16 16l32 0c8.8 0 16-7.2 16-16l0-32c0-8.8-7.2-16-16-16l-32 0c-8.8 0-16 7.2-16 16z"/></svg>');
       background-repeat: no-repeat;
       background-position: center;
@@ -179,7 +179,7 @@ const S = {};
 
   S.Portfolio = styled.div`
     & label::after {
-      content: '+자료첨부';
+      /* content: '+자료첨부'; */
       cursor: pointer;
       font-size: 17px;
       margin-left: 8px;
